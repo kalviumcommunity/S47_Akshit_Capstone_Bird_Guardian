bd3d7b5957236b43e9d501fbdd30c84fa354c74c
aki <akshitpjt07@gmail.com>
2024-06-16 12:58:02 +0530

    InitializedReactApp

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <>
+      <h1>CapStone project</h1>
+      <h2>Project Name: Bird Guardian</h2>
+      <h3>Initialized a react/frontend application</h3>
+    </>
+  )
+}
+
+export default App
