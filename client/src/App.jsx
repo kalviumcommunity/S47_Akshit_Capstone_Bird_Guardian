a10ab1ada9fcf9f53b84baf1da80f6196a352bad
aki <akshitpjt07@gmail.com>
2024-06-23 20:36:59 +0530

    bcryptFunctionAdded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import MainHomePage from "./components/MainHomePage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainHomePage from './components/MainHomePage';
+import AllPost from './componentRelatedToFunctionality/AllPost'; 
+import CreatePost from './componentRelatedToFunctionality/CreatePost'; 
 
 const App = () => {
   return (
-    <>
-      <MainHomePage/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainHomePage />} />
+        <Route path="/AllPost" element={<AllPost />} />
+        <Route path="/CreatePost" element={<CreatePost />} />
+      </Routes>
+    </Router>
   );
 };
 
