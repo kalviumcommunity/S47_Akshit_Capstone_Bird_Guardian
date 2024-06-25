b68bba61be99af3232f112cc1e833c1ab745e001
aki <akshitpjt07@gmail.com>
2024-06-25 23:49:30 +0530

    JWT

--- server/models/User.js
+++ server/models/User.js
@@ -27,12 +27,38 @@ toObject: {
 
 // secure the password with the bcrypt
 UserSchema.pre('save', async function (next) {  // Pre-save middleware means that we wrote 'save' and outside that pre means before the data is saved in the database, the function will run (which fn? => function()...)
-  if (!this.isModified('password')) {  // here this refers to the data (in object like name : "ram", email :"...", password: "....")
+  if (!this.isModified('password')) {  // here this refers to the data (in object like name : "ram", email :"...", password: "....") // already bcrypted 
     next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+
+// Bcrypt
+UserSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+}
+
+
+// JWT
+UserSchema.methods.generateAuthToken = async function () {      // UserSchema.methods via this u can create n no. of new methods, method we made : generateAuthToken
+  try {
+    return jwt.sign(
+      {
+        userId : this._id.toString(),
+        email : this.email,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: '6h'
+      }
+  );
+
+  } catch (error) {
+    console.error('Error generating token:', error);
+  };
+};
 
 
 
